94c309f41f28d9b94de96e761c005e28b3b01304
Zhaoyi Yan <priscillayouziqian@gmail.com>
2025-01-17 21:50:14 -0500

    Express Router

---
+++ routes/campsiteRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const campsiteRouter = express.Router(); //create a express router
+
+//single statement that handles all routings
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+}) // no ; here, because ; stands for end of statement
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+module.exports = campsiteRouter;
